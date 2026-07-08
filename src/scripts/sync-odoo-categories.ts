d146615295a666f74b8566048ad0ebacb42ac338
Zahid M K <91457816+Zahidmk@users.noreply.github.com>
2026-07-08 11:02:57 +0530

    [updt]: category image synch

---
+++ src/scripts/sync-odoo-categories.ts
@@ -0,0 +1,9 @@
+import { ExecArgs } from "@medusajs/framework/types";
+import odooCategorySyncJob from "../jobs/odoo-category-sync-job";
+
+export default async function syncOdooCategories({ container }: ExecArgs) {
+  const logger = container.resolve("logger");
+  logger.info("Starting manual Odoo Category Sync to fetch odoo_ids...");
+  await odooCategorySyncJob(container);
+  logger.info("Odoo Category Sync complete!");
+}
